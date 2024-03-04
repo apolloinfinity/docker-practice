bc9d8039f8d709a033f60a8d80b6b38f394a4273
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2024-03-03 20:43:39 -0800

    WIP: Setting up basic stuff to get app to at least run on the minimum.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors'
+import helmet from 'helmet';
 
 class App {
     public app: express.Application;
@@ -6,6 +9,32 @@ class App {
     public port: string | number;
 
     constructor() {
+        this.app = express();
+        this.env = NODE_ENV || 'development'; // create .env file
 
+        // 
     }
+
+    // Logger function
+
+    public getServer(){
+        return this.app;
+    }
+
+    // Database connection
+
+    private initializeMiddlewares() {
+        this.app.use(morgan(LOG_FORMAT, {stream})); // froms a config file
+        this.app.use(cors({origin: origin})); // might need credentials?
+        this.app.use(helmet());
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: true}));
+    }
+
+    // initialize routes
+
+    // initialize error handling
+
 }
+
+export default App;
